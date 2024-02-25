55f70ef430849dc460b5b0e6c5f295eb1df1a367
Sunjin Jung <jungsunjin9@gmail.com>
2024-02-25 17:11:11 +0900

    [FIX] ex06.cpp 파일의 컴파일 에러 해결

--- ch07_[C++]inheritance/ch07_[C++]inheritance/ex06.cpp
+++ ch07_[C++]inheritance/ch07_[C++]inheritance/ex06.cpp
@@ -1,66 +1,72 @@
 ﻿#include <iostream>
-#include <string> //#include <cstring> 또는 #include <string.h> 와는 다름
+#include <string>
 using namespace std;
-// Item타입 자동형변환, 형변환 연산자
+
 class Item {
-	int num;
-	string name;
 public:
-	Item() {
-		cout << "Item()" << endl;
-	}
-	explicit Item(int num) : num(num) {
-		cout << "Item(int)" << endl;
-	}
-	explict Item(string name) : name(name) {
-		cout << "Item(int)" << endl;
-	}
-	explicit Item(int num, string name) : num(num), name(name) {
-		cout << "Item(int, string)" << endl;
-		return num;
-	}
-	operator int() const {
-		cout << "Item::operator int()" << endl;
-		return num;
-	}
+    Item() {
+        cout << "Item()" << endl;
+    }
+    explicit Item(int num) : num(num) {
+        cout << "Item(int)" << endl;
+    }
+    explicit Item(string name) : name(name) {
+        cout << "Item(string)" << endl;
+    }
+    Item(int num, string name) : num(num), name(name) {
+        cout << "Item(int, string)" << endl;
+    }
+    operator int() const {
+        cout << "Item::operator int()" << endl;
+        return num;
+    }
+    operator string() const {
+        cout << "Item::operator string()" << endl;
+        return name;
+    }
+private:
+    int num;
+    string name;
 };
 
 int main() {
-	cout << "===== A =====" << endl;
-	Item a1 = Item(1);
-	Item a2(2);
-	Item a3 = (Item)3;	//Item(3)와 같음
-	//Item a4 = 4;		//explicit 키워드에 의해 허용을 금함
-	Item a4 = 4;
-	Item a5, a6, a7;
-	a5 = Item(5);
-	//a6 = 6;			//explicit 키워드에 의해 혀용을 금함
-	a7 = (Item)7;
+    cout << "===== A =====" << endl;
+    Item a1 = Item(1);
+    Item a2(2);
+    Item a3 = (Item)3; // Item(3)와 같음
+    //Item a4 = 4; //explicit 키워드에 의해 허용을 금함
 
-	cout << "===== B =====" << endl;
-	Item b4 = string("Stone");
-	Item b6 = (string)"Stone";
+    Item a5, a6, a7;
+    a5 = Item(5);
+    //a6 = 6; //explicit 키워드에 의해 혀용을 금함
+    a7 = (Item)7;
 
-	Item b7 = Item("Stone");
-	Item b8 = (Item)"Stone";
+    cout << "===== B =====" << endl;
+    Item b4 = Item(string("Stone"));
+    Item b6 = Item((string)"Stone");
 
-	cout << "===== C =====" << endl;
-	Item c1 = Item(1, "Stone");
-	Item c2(2, "Dirt");
-	//Item c4 = { 3, "Wood" };
-	Item c5, c6;
-	c5 = Item(4, "Grass");
-	//c6 = { 5, "Water" };
+    Item b7 = Item("Stone");
+    Item b8 = (Item)"Stone";
 
-	cout << "===== D =====" << endl;
-	Item i1(1, "Stone");
-	int inum = i1;
-	cout << inum << endl;
-	string iname = i1;
-	cout << iname << endl;
+    cout << "===== C =====" << endl;
+    Item c1 = Item(1, "Stone");
+    Item c2(2, "Dirt");
+    //Item c4 = { 3, "Wood" };
+    Item c5, c6;
+    c5 = Item(4, "Grass");
+    //c6 = { 5, "Water" };
 
-	int inum = (int)i1;
-	cout << inum << endl;
-	string iname = (string)i1;
-	cout << iname << endl;
+    cout << "===== D =====" << endl;
+    Item i1(1, "Stone");
+    int inum = i1;
+    cout << inum << endl;
+    string iname = i1;
+    cout << iname << endl;
+
+    inum = (int)i1;
+    cout << inum << endl;
+    iname = (string)i1;
+    cout << iname << endl;
+
+    return 0;
 }
